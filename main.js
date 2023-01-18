126f0c8751c138a15981c3e37b2a7d735f0863da
darrell <jafet.dna@gmail.com>
2023-01-17 19:00:57 -0600

    final

--- main.js
+++ main.js
@@ -6,35 +6,57 @@
 
 // Colocar la respuesta acá
 
-// ====================================================================================================
+los datos primitivos, se podria decir que son los mas basicos a la hora de hablar del lenguage de JS. aqui podemos encontrar lo que son los  //string, number y boolean.
+
+ejemplos:
+
+String: ("hola mundo");
+number: se pueden ver como simples numeros flotantes /123 o con la funcion number();
+boolean: este se usa para representar valores ("true") o ("false")
 
 
 // 2. Respuesta pregunta 2
 
 // Colocar la respuesta acá
 
-// ====================================================================================================
+1) notacion de puntos ("objeto.propiedad = valor")
+2) con corchetes("objeto[propiedad]= valor")
+
 
 
 // 3. Respuesta pregunta 3
 
 // Colocar la respuesta acá
 
-// ====================================================================================================
-
-
+(1)en el caso uno lo que hace es sumar los valores, pues +=  suma el valor del operando derecho a una variable y asigna el resultado a la variable
+(2) el segundo caso es invalido
 // 4. Respuesta pregunta 4
 
 // Colocar la respuesta acá
 
-// ====================================================================================================
+A.
+const flag = false;
+
+flag != true; // luego que flag es !=( no es igual estricto) a true y es validdo
+
+flag !== true;// comprueba si sus dos operandos no son iguales, devolviendo un resultado booleano
+
+B.
+const num = 10;
+
+num != '10'; //en este caso va a devolver 10 pues el numero que esta al lado de la operacion es un string 
+
+num !== '10';
+
+
+
 
 
 // 5. Respuesta pregunta 5
 
 // Colocar la respuesta acá
 
-// ====================================================================================================
+son la funciones que no han sido declaradas con un nombre 
 
 
 // 6. Respuesta pregunta 6
@@ -48,23 +70,29 @@
 
 // Colocar la respuesta acá
 
-// ====================================================================================================
+// es una función la cual es propiedad de un Objeto
 
 
 // 8. Respuesta pregunta 8
 
 // Colocar la respuesta acá
 
-// ====================================================================================================
-
+// es una funcion llamandose asi misma
 
 // II Parte: Ejercicios
 
 // 1. Respuesta pregunta 1
 
 // Colocar el código acá
+num_aleatorios = [7, 8, 5, 10, 11, 9, 2, 4]
 
-// ====================================================================================================
+
+lista_pares = []
+lista_impares = []
+
+for (numero = num_aleatorios){
+    
+}
 
 
 // 2. Respuesta pregunta 2
